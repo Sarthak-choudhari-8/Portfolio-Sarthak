9303f84d6708212e9a3f0000ee8534a96c4380ab
Sarthak Choudhari <142074822+Sarthak-choudhari-8@users.noreply.github.com>
2024-02-18 00:30:27 +0530

    trail 4

---
+++ index.js
@@ -0,0 +1,5 @@
+
+let btn  =  document.querySelector(".send-mail-button");
+btn.addEventListener("click" , ()=>{console.log("btn clicked "); 
+// alert("Message was send Successfully , I will reply you soon .");
+});
